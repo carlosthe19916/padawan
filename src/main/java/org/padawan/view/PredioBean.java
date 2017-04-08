02238012dfe5f2e8e5aa234eb162b338c66c9673
carlos <carlosthe19916@gmail.com>
2017-04-08 17:17:41 -0500

    add basic template

--- src/main/java/org/padawan/view/PredioBean.java
+++ src/main/java/org/padawan/view/PredioBean.java
@@ -2,19 +2,31 @@ package org.padawan.view;
 
 import org.padawan.service.PredioService;
 
+import javax.annotation.ManagedBean;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
 @Named
 @RequestScoped
+@ManagedBean
 public class PredioBean {
+
+    private String title = "Hola";
 
     @Inject
     PredioService predioService;
 
     public void llamarService() {
+        System.out.println("Entro al PREDIO BEAN");
         this.predioService.llamarStoreProcedure();
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
